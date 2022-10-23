ad8207fd10bee9445d77b5d1dea615ab8c8a4756
rajarai007 <rajabahadurrai@gmail.com>
2022-10-23 12:25:48 +0530

    1

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+   let a="";
+  for (let i =N-1;i>=0;i--){
+      a=a+str[i];
+  }
+  if(a==str){
+      console.log("Yes")
+  }else{
+      console.log("No")
+  }
---
+++ prime.js
@@ -0,0 +1,8 @@
+let IsPrime=1;
+    for(let i=0;i<=num;i++){
+        if(IsPrime%num==0 || IsPrime%1==0){
+            console.log("Yes")
+        }else{
+            console.log("No")
+        }
+    }
